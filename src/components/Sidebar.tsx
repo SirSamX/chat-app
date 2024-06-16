39d09097ac570cdd00bd0bf7570a5488a4bd862c
SirSamX <samuelheil@web.de>
2024-06-16 16:29:16 +0200

    Some changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ChatPreview from "./ChatPreview";
 import { saveChats } from "@/app/api/save";
 import { fetchChats } from "@/app/api/load";
 
 
 export default function Sidebar() {
+  const chatNameInput = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
-    fetchChats().then(result => setChats(result.map(chat => chat.chat)))
-    
+    console.log("1")
+    fetchChats()
+      .then(result => setChats(result.map(chat => chat.chat)))
+      .catch(error => console.error(error))
   }, []);
 
-  const [chats, setChats] = useState(["Chat 1"])
+  const [chats, setChats] = useState<string[]>([])
 
   function addChat() {
-    const chatName = "Chat " + (chats.length + 1)
+    if (chatNameInput.current == null) return
+    let chatName = chatNameInput.current.value
+    chatName = chatName || "New Chat";
+    chatNameInput.current.value = ""
     setChats([...chats, chatName])
     saveChats(chatName)
   }
@@ -26,12 +32,20 @@ export default function Sidebar() {
       <div className="flex items-center justify-between p-2 border-b border-gray-300 dark:border-gray-700">
         <h2 className="text-xl font-bold text-gray-900 dark:text-gray-100">Chats</h2>
         
-        <button
-          className="text-gray-600 dark:text-gray-400"
-          onClick={addChat}
-        >
-          +
-        </button>
+        <div>
+          <input
+            ref={chatNameInput}
+            type="text"
+            placeholder="New Chat"
+            className="p-1 border rounded-md dark:bg-gray-800 dark:text-gray-100 outline-none mr-4"
+          />
+          <button
+            className="text-gray-600 dark:text-gray-400"
+            onClick={addChat}
+          >
+            +
+          </button>
+        </div>
       </div>
 
       <div className="mt-4 test">
